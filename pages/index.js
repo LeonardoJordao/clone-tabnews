89aa32587f662ce107195201e1cccd59bd2f4ec1
LeonardoJordao <159403440+LeonardoJordao@users.noreply.github.com>
2024-09-12 23:43:31 +0000

    Altera texto da mensagem na página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>Mar calmo nunca fez bom navegador.</h1>;
+  return (
+    <h1>
+      🦉 Tente aprender alguma coisa sobre tudo e tudo sobre alguma coisa.
+    </h1>
+  );
 };
 
 export default Home;
