471c2fa63520a51eed2c7c2f31012b0e4538d60a
LeonardoJordao <159403440+LeonardoJordao@users.noreply.github.com>
2024-10-01 03:35:06 +0000

    Alterado endpoint status para obter dados do database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,56 @@
 import database from "infra/database.js";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ msg: "testa criação de endpoint - fantástico" });
+  const updatedAt = new Date().toISOString();
+  const dbData = await database.query(
+    `SELECT version, max_connections, used_connections
+     FROM
+      (SELECT version() AS version) t1,
+      (SELECT sum(numbackends)::int AS used_connections FROM pg_stat_database WHERE datname is not null) t2,
+      (SELECT setting::int AS max_connections FROM pg_settings WHERE name = 'max_connections') t3
+    ;`,
+  );
+  const dbVersionValue = await database.query("SHOW server_version;");
+
+  // const dbVersion = dbData.rows[0].version;
+  const dbVersion = dbVersionValue.rows[0].server_version;
+  const dbMaxConnections = dbData.rows[0].max_connections;
+  const dbUsedConnections = dbData.rows[0].used_connections;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: dbMaxConnections,
+        used_connections: dbUsedConnections,
+      },
+    },
+  });
 };
 
 export default status;
+
+// const maxConnections = await database.query(
+//   "SELECT setting as max_connections FROM pg_settings WHERE name = 'max_connections';",
+// );
+// console.log(maxConnections.rows);
+
+// const usedConnections = await database.query(
+//   // "SELECT count(*) FROM pg_stat_activity WHERE datname is not null;",
+//   "SELECT sum(numbackends) as used_connections FROM pg_stat_database WHERE datname is not null;",
+// );
+// console.log(usedConnections.rows);
+
+// const pgVersion = await database.query("SELECT version() pg_version;");
+// console.log(pgVersion.rows[0].pg_version);
+
+// const listConnections = await database.query(
+//   `select max_conn,used,res_for_super,max_conn-used-res_for_super res_for_normal
+//    from
+//     (select count(*) used from pg_stat_activity) t1,
+//     (select setting::int res_for_super from pg_settings where name=$$superuser_reserved_connections$$) t2,
+//     (select setting::int max_conn from pg_settings where name=$$max_connections$$) t3
+//   ;`,
+// );
+// console.log(listConnections.rows);
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,35 @@
+let responseBody;
+
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  responseBody = await response.json();
   expect(response.status).toBe(200);
+});
+
+test("updated_at should be a date", async () => {
+  const updated_at = responseBody.updated_at;
+  expect(updated_at).toBeDefined();
+  const parsedDate = new Date(updated_at).toISOString();
+  expect(updated_at).toEqual(parsedDate);
+  // console.log(updated_at);
+});
+
+test("Database version should be defined", async () => {
+  const dbVersion = responseBody.dependencies.database.version;
+  expect(dbVersion).toBeDefined();
+  // console.log(dbVersion);
+});
+
+test("Database max_connections should be an integer", async () => {
+  const max_connections = responseBody.dependencies.database.max_connections;
+  expect(max_connections).toBeDefined();
+  expect(typeof max_connections).toBe("number");
+  // console.log(max_connections);
+});
+
+test("Database used_connections should be an integer", async () => {
+  const used_connections = responseBody.dependencies.database.used_connections;
+  expect(used_connections).toBeDefined();
+  expect(typeof used_connections).toBe("number");
+  console.log(used_connections);
 });
