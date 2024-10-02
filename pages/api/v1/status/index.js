9704b4b1c796d1bbf61699c5ef40dbee6fd324a0
LeonardoJordao <159403440+LeonardoJordao@users.noreply.github.com>
2024-10-02 01:38:48 +0000

    Finaliza endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,19 +1,21 @@
 import database from "infra/database.js";
 
 const status = async (req, res) => {
+  const databaseName = process.env.POSTGRES_DB;
   const updatedAt = new Date().toISOString();
-  const dbData = await database.query(
-    `SELECT version, max_connections, used_connections
+  const dbData = await database.query({
+    text: `SELECT version, max_connections, used_connections
      FROM
       (SELECT version() AS version) t1,
-      (SELECT sum(numbackends)::int AS used_connections FROM pg_stat_database WHERE datname is not null) t2,
+      (SELECT count(*)::int AS used_connections FROM pg_stat_activity WHERE datname = $1) t2,
       (SELECT setting::int AS max_connections FROM pg_settings WHERE name = 'max_connections') t3
     ;`,
-  );
+    values: [databaseName],
+  });
   const dbVersionValue = await database.query("SHOW server_version;");
 
-  // const dbVersion = dbData.rows[0].version;
   const dbVersion = dbVersionValue.rows[0].server_version;
+  // const dbVersion = dbData.rows[0].version;
   const dbMaxConnections = dbData.rows[0].max_connections;
   const dbUsedConnections = dbData.rows[0].used_connections;
 
@@ -30,27 +32,3 @@ const status = async (req, res) => {
 };
 
 export default status;
-
-// const maxConnections = await database.query(
-//   "SELECT setting as max_connections FROM pg_settings WHERE name = 'max_connections';",
-// );
-// console.log(maxConnections.rows);
-
-// const usedConnections = await database.query(
-//   // "SELECT count(*) FROM pg_stat_activity WHERE datname is not null;",
-//   "SELECT sum(numbackends) as used_connections FROM pg_stat_database WHERE datname is not null;",
-// );
-// console.log(usedConnections.rows);
-
-// const pgVersion = await database.query("SELECT version() pg_version;");
-// console.log(pgVersion.rows[0].pg_version);
-
-// const listConnections = await database.query(
-//   `select max_conn,used,res_for_super,max_conn-used-res_for_super res_for_normal
-//    from
-//     (select count(*) used from pg_stat_activity) t1,
-//     (select setting::int res_for_super from pg_settings where name=$$superuser_reserved_connections$$) t2,
-//     (select setting::int max_conn from pg_settings where name=$$max_connections$$) t3
-//   ;`,
-// );
-// console.log(listConnections.rows);
