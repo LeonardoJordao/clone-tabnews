270b1c9ff3bb4727cc22127580db44754db76c11
LeonardoJordao <159403440+LeonardoJordao@users.noreply.github.com>
2024-10-12 01:06:30 +0000

    Adicionada possibilidade de uso certificado autoassinado

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,9 @@ const query = async (queryObject) => {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV == "development" ? false : true,
+    ssl: getSSLValues(),
   });
-  console.log("Credenciais do postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -32,3 +26,13 @@ const query = async (queryObject) => {
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV == "development" ? false : true;
+}
