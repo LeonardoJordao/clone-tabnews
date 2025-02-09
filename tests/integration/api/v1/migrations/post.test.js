fe8f0965cba9eae624ebebba3462abe31645e98d
LeonardoJordao <159403440+LeonardoJordao@users.noreply.github.com>
2025-02-09 23:25:51 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -8,26 +8,39 @@ beforeAll(async () => {
 
 let responseBody = undefined;
 
-test("POST to api/v1/migrations should return 200", async () => {
-  // PRIMEIRO POST - RODA A MIGRATION
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("At the first time", async () => {
+        // PRIMEIRO POST - RODA A MIGRATION
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        //espera uma lista de migrations
+        responseBody = await response1.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("At the second time", async () => {
+        // SEGUNDO POST - NÃO DEVE HAVER MIGRATION PARA EXECUTAR
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        //espera uma lista de migrations VAZIA
+        responseBody = await response2.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  //espera uma lista de migrations
-  responseBody = await response1.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  // SEGUNDO POST - NÃO DEVE HAVER MIGRATION PARA EXECUTAR
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  //espera uma lista de migrations VAZIA
-  responseBody = await response2.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,39 +6,45 @@ beforeAll(async () => {
 
 let responseBody = undefined;
 
-test("GET to api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-});
+describe("GET api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+    });
 
-test("GET to api/v1/status should have a response body", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  responseBody = await response.json();
-  expect(responseBody).toBeDefined();
-});
+    test("Receive a response body", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      responseBody = await response.json();
+      expect(responseBody).toBeDefined();
+    });
 
-test("updated_at should be a date", async () => {
-  const updated_at = responseBody.updated_at;
-  const parsedDate = new Date(updated_at).toISOString();
-  expect(updated_at).toEqual(parsedDate);
-  // console.log(updated_at);
-});
+    test("Receive date of update", async () => {
+      const updated_at = responseBody.updated_at;
+      const parsedDate = new Date(updated_at).toISOString();
+      expect(updated_at).toEqual(parsedDate);
+      // console.log(updated_at);
+    });
 
-test("Database version should be defined", async () => {
-  const dbVersion = responseBody.dependencies.database.version;
-  expect(dbVersion).toBeDefined();
-  // console.log(dbVersion);
-});
+    test("Receive database version", async () => {
+      const dbVersion = responseBody.dependencies.database.version;
+      expect(dbVersion).toBeDefined();
+      // console.log(dbVersion);
+    });
 
-test("Database max_connections should be an integer", async () => {
-  const max_connections = responseBody.dependencies.database.max_connections;
-  expect(typeof max_connections).toBe("number");
-  // console.log(max_connections);
-});
+    test("Receive database max_connections", async () => {
+      const max_connections =
+        responseBody.dependencies.database.max_connections;
+      expect(typeof max_connections).toBe("number");
+      // console.log(max_connections);
+    });
 
-test("Database used_connections should be 1", async () => {
-  const used_connections = responseBody.dependencies.database.used_connections;
-  expect(used_connections).toEqual(1);
-  // expect(typeof used_connections).toBe("number");
-  // console.log(used_connections);
+    test("Receive database used_connections", async () => {
+      const used_connections =
+        responseBody.dependencies.database.used_connections;
+      expect(used_connections).toEqual(1);
+      // expect(typeof used_connections).toBe("number");
+      // console.log(used_connections);
+    });
+  });
 });
